1820ce2cb526aefbc590f37d0bbf9fde00110e32
Algebra-FUN <worldcreator@foxmail.com>
2022-08-26 22:30:21 +0800

    delete js in string

--- WeReadScan/WeRead.py
+++ WeReadScan/WeRead.py
@@ -128,44 +128,6 @@ class WeRead:
         self.S('li.chapterItem:nth-child(2)').click()
 
     def inject_observer_js(self):
-        # js = '''
-        # const rootElement = document.createElement("html");
-        # const styleElement = document.createElement("style");
-        # const bodyElement = document.createElement("body");
-        # rootElement.append(styleElement);
-        # rootElement.append(bodyElement);
-
-        # // define observer handlers
-        # const contentObserver = new MutationObserver((_, observer) => {
-        # const content = document.querySelector(".preRenderContainer:not([style])");
-        # if (content) {
-        #     // define styles
-        #     if (styleElement.childNodes.length === 0) {
-        #     const contentStyle = content.querySelector("style");
-        #     if (contentStyle?.childNodes.length) {
-        #         styleElement.innerHTML = contentStyle.innerHTML
-        #         .replaceAll(".readerChapterContent", ".preRenderContent")
-        #         .replaceAll(/汉仪旗黑(?=\d)/g, "汉仪旗黑 ")
-        #         .replaceAll(/汉仪楷体(?!S)/g, "汉仪楷体S");
-        #         styleElement.append(
-        #         ".preRenderContent { page-break-after: always; }"
-        #         );
-        #     }
-        #     }
-        #     // append contents
-        #     const contentDiv = content.querySelector("#preRenderContent");
-        #     if (contentDiv) {
-        #     contentDiv.removeAttribute("id");
-        #     contentDiv
-        #         .querySelectorAll("img")
-        #         .forEach(
-        #         (img) => (img.src = img.getAttribute("data-src") || img.src)
-        #         );
-        #     bodyElement.append(contentDiv.cloneNode(true));
-        #     }
-        # }
-        # })
-        # '''
         self.driver.execute_cdp_cmd(
             'Page.addScriptToEvaluateOnNewDocument', {'source': self.inject_js})
 
